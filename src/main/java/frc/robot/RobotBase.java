e78bc2c49d7497a97dfe74a5ee72e173575a6a6f
declan <dmcelligott@aimpa.org>
2020-02-22 12:56:52 -0500

    Revert "Revert "fixed some of the controller inputs and actions""
    This reverts commit e7482f81372e92a621d16a8f7805bef2c46eb534.

--- src/main/java/frc/robot/RobotBase.java
+++ src/main/java/frc/robot/RobotBase.java
@@ -87,24 +87,8 @@ public class RobotBase {
 		else {
 			motIntake.set(0.0);
 		}
-
-
-		/**
-		if(inputs.bMouthIn == true) {
-			motMouth.set(1); 
-		}
-		else if(inputs.bMouthOut == true)
-		{
-			motMouth.set(-1); 
-		}
-		else 
-		{
-			motMouth.set(0); 
-		}
-		**/
 		//Setting Intake Soloniod to true/false
 		solTeainator.set(inputs.bTeainatorToggle);
-
     }
 
     public void addTelemetryHeaders(LCTelemetry telem ){
@@ -136,8 +120,5 @@ public class RobotBase {
 	    	SmartDashboard.putNumber("O_>>>Motors", dRightDrivePower);
     	}
 
-	}
-
-   
-    
+	}   
 }
